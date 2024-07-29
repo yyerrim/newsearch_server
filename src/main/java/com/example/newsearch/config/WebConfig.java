82f991b231bea48fa49513598e18639d67c77638
youngjun kim <youngjun4327@gmail.com>
2024-07-29 12:18:30 +0900

    addCorsMappings 수정

--- src/main/java/com/example/newsearch/config/WebConfig.java
+++ src/main/java/com/example/newsearch/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.example.newsearch.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,18 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://newsearch.site")  // 허용할 도메인 설정
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true)
-                        .maxAge(3600);
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://main--newsearch1.netlify.app", "https://newsearch.site")  // 허용할 도메인 배열로 설정
+
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
